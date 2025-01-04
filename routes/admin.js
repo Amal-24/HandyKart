ac2a93e1eb51cc7481c1d6b292515d7d35e817f5
Amal Krishna <amalkrishnaprasad4@gmail.com>
2025-01-04 19:00:43 +0530

    updated

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,13 @@
 var express = require("express");
 var router = express.Router();
 var productHelpers = require("../helpers/productHelpers");
+let fs=require("fs-extra")
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
 
   productHelpers.viewProduct().then((products)=>{
-    res.render("admin/view_products", { admin: true, products});
+    res.render("admin/admin_home", { admin: true, products});
 
   })
   
@@ -51,15 +52,22 @@ router.get('/delete_product',(req,res)=>{
 
 
 router.get('/edit_product',async(req,res)=>{
-  let id=req.query.id
-  let product= await productHelpers.getProductDetails(id)
+  let product_id=req.query.id
+  let product= await productHelpers.getProductDetails(product_id)
   res.render('admin/edit_product',{admin:true,product})}
 );
 
 router.post('/edit_product',async(req,res)=>{
-  let id=req.query.id
+  let product_id=req.query.id
+  console.log('a61',product_id)
   req.body.Price=parseInt(req.body.Price)
-  let updateResult= await productHelpers.updateProduct(id,req.body)
+  if(req.body.Condition=='true'){
+    let delete_image=await fs.unlinkSync("../public/product_images/"+product_id+".jpg")
+    console.log('a66',delete_image)
+   let image = req.files.Image;
+   let replaced_image=await image.mv("../public/product_images/" + product_id + ".jpg")
+  }
+  let updateResult= await productHelpers.updateProduct(product_id,req.body)
   res.redirect('/admin')
 })
 
